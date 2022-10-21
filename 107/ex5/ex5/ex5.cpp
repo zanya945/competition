9a0286f899563a349a073c658069b34bc658aaf4
zanya945 <tea915656@gmail.com>
2022-10-21 15:39:22 +0800

    new

---
+++ 107/ex5/ex5/ex5.cpp
@@ -0,0 +1,20 @@
+﻿#define fa ios::sync_with_stdio(false); cin.tie(NULL);
+#define N 1000050 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int in_count = 0;
+    vector<int> arr = vector<int>(N);
+    cin >> in_count;
+    for (int i = 0; i < in_count; i++) {
+        int num = 0;
+        cin >> num;
+        arr.push_back(i);
+    }
+
+    return 0;
+}
